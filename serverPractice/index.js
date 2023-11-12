951184af514f88d2f9fb3b67440f24f04794476c
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-11-12 07:39:05 +0530

    working stiill

--- serverPractice/index.js
+++ serverPractice/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const userRouter = require('./routes/user')
+const adminRouter = require('./routes/admin')
 const app = express()
 
 app.use(express.json())
 
 app.use('/user', userRouter)
+app.use('/admin', adminRouter)
 
 mongoose.connect("mongodb://127.0.0.1:27017/courses")
 
---
+++ serverPractice/routes/admin.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { authenticateJwt, SECRETKEY } = require('../middleware/auth')
+const { Admin, Course } = require('../db/model')
+const jwt = require('jsonwebtoken')
+const router = express.Router()
+
+router.get('/me', authenticateJwt, async (req, res) => {
+    const admin = await Admin.findOne({ username: req.user.username })
+    if (!admin) {
+        res.status(404).json({ mssage: "Admin doesnt exist" })
+    }
+    res.json({
+        username: admin.username
+    })
+})
+
+router.post("/signup", async (req, res) => {
+    const { username, password } = req.body
+    const admin = await Admin.findOne({ username })
+    if (admin) {
+        res.json({
+            message: "Admin already exists"
+        })
+    }
+    const newAdmin = new Admin({ username, password })
+    await newAdmin.save()
+    const token = jwt.sign({ username, role: "admin" }, SECRETKEY, { expiresIn: '7d' })
+    res.status(200).json({ message: "Admin created successfully", token })
+})
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body
+    const admin = await Admin.findOne({ username })
+    if (!admin) {
+        res.status(311).json({ message: "invalid username or password" })
+    }
+    const token = jwt.sign({ username, role: "admin" }, SECRETKEY, { expiresIn: '7d' })
+    res.json({ message: "logged in successfully", token })
+})
+
+router.post("/course", authenticateJwt, async(req,res)=>{
+    const course = new Course(req.body)
+    await course.save()
+    res.json({message:"Course created successfully", courseId:course.id})
+}
+)
+module.exports = router
